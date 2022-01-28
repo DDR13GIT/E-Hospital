a444bca1dbd183f1f60fe685e56886a150f82dd1
Noumi <isratmoyeen.23@gmail.com>
2022-01-29 02:15:58 +0600

    Feat:Login Added If user inserts correct username and password ,The system will take her to the dashboard screen

--- src/main/java/com/example/ehospital/MainController.java
+++ src/main/java/com/example/ehospital/MainController.java
@@ -1,14 +1,85 @@
 package com.example.ehospital;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.io.IOException;
+import java.sql.*;
+import java.util.EventObject;
 
 public class MainController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public TextField IdField;
+    @FXML
+  public PasswordField  PasswordField;
+    @FXML
+    public AnchorPane loginpane;
+    public Button SigninBtn;
+    PreparedStatement pst = null;
+    ResultSet rs;
+
+
+
+    public void SignInAction(ActionEvent actionEvent) {
+        System.out.println(IdField.getText());
+        System.out.println(PasswordField.getText());
+
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            String url = "jdbc:sqlserver://localhost:1433;user=team;password=p@ssword13;databaseName=EHdb";
+            Connection con = DriverManager.getConnection(url);
+            String uname = IdField.getText();
+            String pass =  PasswordField.getText();
+
+
+            System.out.println("printing from here:"+ uname);
+            System.out.println("printing from here:"+ pass);
+
+            if (uname.equals("") || pass.equals("")) {
+//                JOptionPane.showMessageDialog(null,);
+                System.out.println("Please Fill up all the information");
+
+
+            } else {
+                String query = "SELECT * FROM  LoginInfo WHERE UserName=? and Password=?";
+
+
+                pst = con.prepareStatement(query);
+                pst.setString(1, uname);
+                pst.setString(2, pass);
+                rs = pst.executeQuery();
+                if (rs.next()) {
+                    Parent root = FXMLLoader.load(getClass().getResource("Dashboard.fxml"));
+                    Stage stage = (Stage)(Window)SigninBtn.getScene().getWindow();
+                    Scene scene = new Scene(root);
+                    stage.setScene(scene);
+                    stage.show();
+
+
+                } else {
+                    System.out.println("Please Enter Correct email Or Password");
+                }
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.ehospital {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires  mssql.jdbc;
 
 
     opens com.example.ehospital to javafx.fxml;
