3fcdc9294894d4d2e529d576199bb7c82599f096
DDR13GIT <ddroy13@gmail.com>
2022-01-28 20:02:34 +0600

    core: changes in build.gradle and main java file renamed for clarity

--- src/main/java/com/example/ehospital/Main.java
+++ src/main/java/com/example/ehospital/Main.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("signIn.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("signIn.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        stage.setTitle("E-Hospital-Hospital Management System");
         stage.setScene(scene);
         stage.show();
         stage.setMaximized(true);
