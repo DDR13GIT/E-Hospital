bb77bedb9249e53e729f86d5c3dc243831efa55d
Noumi <isratmoyeen.23@gmail.com>
2022-02-12 00:09:59 +0600

    Feat:Insert Update Refresh Added

--- src/main/java/com/example/ehospital/DepartmentController.java
+++ src/main/java/com/example/ehospital/DepartmentController.java
@@ -2,20 +2,31 @@ package com.example.ehospital;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import org.controlsfx.control.Notifications;
 
 import java.net.URL;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ResourceBundle;
+
+import static java.time.zone.ZoneRulesProvider.refresh;
 
 public class DepartmentController implements Initializable {
     ObservableList<DepartmentTableModel> departmentList = FXCollections.observableArrayList();
+    @FXML
+    public TextField Deptnamefxid;
+    @FXML
+    public TextField Des_fxid;
+    @FXML
+    public TextField Status_fxid;
+    @FXML
+    public TextField DeptIdid;
 
     @FXML
     private TableView<DepartmentTableModel> DepartmentTableFxid;
@@ -33,6 +44,7 @@ public class DepartmentController implements Initializable {
 
     @FXML
     private TableColumn<DepartmentTableModel, String> SerialNoFxid;
+    DepartmentTableModel dept;
 
     Connection conn;
 
@@ -72,5 +84,87 @@ public class DepartmentController implements Initializable {
 
     }
 
+    public void InsertBtn(ActionEvent actionEvent)  throws SQLException {
+        PreparedStatement pst = null;
+        try {
+            String query = " INSERT INTO Department ( DepartmentName, Description, Status) VALUES(?,?,?)";
+
+            pst = conn.prepareStatement(query);
+            String depname=Deptnamefxid.getText();
+            String des= Des_fxid.getText();
+            String sta= Status_fxid.getText();
+
+            if(depname.equals("") || des.equals("")||sta.equals(""))
+            {
+                Notifications.create()
+                        .title("Warning")
+                        .text("Please fillup all the information")
+                        .showError();
+
+            }
+            else
+            {
+
+                pst.setString(1, depname);
+                pst.setString(2, des);
+                pst.setString(3, sta);
+
+
+
+
+                pst.executeUpdate();
+                Notifications.create()
+                        .title("Info")
+                        .text("Added Successfully")
+                        .show();
+                System.out.println("inserted");
+
+
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        
+        }
+
+        }
+
+
+    public void UpdateBtn(ActionEvent actionEvent) {
+    }
+
+    public void DeleteBtn(ActionEvent actionEvent) {
+        PreparedStatement pst = null;
+        Connection con;
+        try {
+
+            dept=DepartmentTableFxid.getSelectionModel().getSelectedItem();
+            String sql="DELETE FROM Department  WHERE Department.DepartmentName=?";
+            pst = conn.prepareStatement(sql);
+            pst.setString(1,dept.getDepartmentName());
+            pst.executeUpdate();
+            Notifications.create()
+                    .title("Info")
+                    .text("Deleted Successfully")
+                    .show();
+
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+
+    }
+
+    public void ResetBtn(ActionEvent actionEvent) throws SQLException {
+
+        departmentList.clear();
+        fetch_info();
+
+
+
+    }
 }
+
 
