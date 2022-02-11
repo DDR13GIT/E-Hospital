9a7d2b4ffc22bbb18a779ca72452c417fbcb2682
DDR13GIT <ddroy13@gmail.com>
2022-02-11 16:35:11 +0600

    Small changes in admin dashboard

--- src/main/java/com/example/ehospital/AdminDashboardController.java
+++ src/main/java/com/example/ehospital/AdminDashboardController.java
@@ -2,7 +2,6 @@ package com.example.ehospital;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 
@@ -14,14 +13,17 @@ public class AdminDashboardController {
         Fxmlloader object = new Fxmlloader();
         Pane view = object.getPage("department");
         mainPane.setCenter(view);
-
-
-
     }
 
     public void doctorBtn(ActionEvent actionEvent) {
         Fxmlloader object = new Fxmlloader();
         Pane view = object.getPage("DoctorList");
+        mainPane.setCenter(view);
+    }
+
+    public void patientBtn(ActionEvent actionEvent) {
+        Fxmlloader object = new Fxmlloader();
+        Pane view = object.getPage("PatientList");
         mainPane.setCenter(view);
     }
 }
