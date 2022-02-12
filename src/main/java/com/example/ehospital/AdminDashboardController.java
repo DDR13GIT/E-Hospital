9d4d2f1b3c441dba04a208006138f8a295aba1c9
DDR13GIT <ddroy13@gmail.com>
2022-02-12 18:00:05 +0600

    Feat: Design changes in fxml

--- src/main/java/com/example/ehospital/AdminDashboardController.java
+++ src/main/java/com/example/ehospital/AdminDashboardController.java
@@ -1,51 +1,60 @@
 package com.example.ehospital;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class AdminDashboardController {
-    @FXML
-    private BorderPane mainPane;
-    public Button departmentBtnFxid;
 
-    public void departmentBtn(ActionEvent actionEvent) throws IOException {
-       Fxmlloader object = new Fxmlloader();
-//        Pane view = object.getPage("department");
-//        mainPane.setCenter(view);
-
-//        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("deparment.fxml")));
-//        Stage stage = (Stage)(Window)departmentBtnFxid.getScene().getWindow();
+//    public void logout(ActionEvent actionEvent) throws IOException {
+//        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Login.fxml"));
 //        Scene scene = new Scene(root);
+//        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
 //        stage.setScene(scene);
 //        stage.show();
+//    }
 
-        Parent root4 = FXMLLoader.load(getClass().getResource("department.fxml"));
-        Scene scene4 = new Scene(root4);
+
+    public void departmentBtn(ActionEvent actionEvent) throws IOException {
+        Parent root1 = FXMLLoader.load(getClass().getResource("department.fxml"));
+        Scene scene1 = new Scene(root1);
         Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        window.setScene(scene4);
+        window.setScene(scene1);
         window.show();
-
     }
 
-    public void doctorBtn(ActionEvent actionEvent) {
-        Fxmlloader object = new Fxmlloader();
-        Pane view = object.getPage("DoctorList");
-        mainPane.setCenter(view);
+    public void doctorBtn(ActionEvent actionEvent)  throws IOException{
+        Parent root1 = FXMLLoader.load(getClass().getResource("DoctorList.fxml"));
+        Scene scene1 = new Scene(root1);
+        Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        window.setScene(scene1);
+        window.show();
     }
 
-    public void patientBtn(ActionEvent actionEvent) {
-        Fxmlloader object = new Fxmlloader();
-        Pane view = object.getPage("PatientList");
-        mainPane.setCenter(view);
+    public void patientBtn(ActionEvent actionEvent) throws IOException {
+        Parent root1 = FXMLLoader.load(getClass().getResource("PatientList.fxml"));
+        Scene scene1 = new Scene(root1);
+        Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        window.setScene(scene1);
+        window.show();
+    }
+    public void appointmentBtn(ActionEvent actionEvent) throws IOException {
+        Parent root1 = FXMLLoader.load(getClass().getResource("Appointment.fxml"));
+        Scene scene1 = new Scene(root1);
+        Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        window.setScene(scene1);
+        window.show();
+    }
+    public void billingBtn(ActionEvent actionEvent) throws IOException {
+        Parent root1 = FXMLLoader.load(getClass().getResource("Billing.fxml"));
+        Scene scene1 = new Scene(root1);
+        Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        window.setScene(scene1);
+        window.show();
     }
 }
